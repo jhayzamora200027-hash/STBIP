bb0cbe599cdf0becd46803422116fe8227ba7a91
Jay-ar Zamora <jhayzamora200027@gmail.com>
2026-03-31 14:33:47 +0800

    (1)

---
+++ public/js/philippines-map-mobile.js
@@ -0,0 +1,9 @@
+
+(function () {
+    if (typeof window.__phil_map_mobile_loaded === 'undefined') {
+        window.__phil_map_mobile_loaded = true;
+        try {
+            console.log('philippines-map-mobile: fallback loaded');
+        } catch (e) {}
+    }
+})();
